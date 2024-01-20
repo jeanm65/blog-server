e0e4f5fce022e32abf58428b450510ca8720f318
JeanM65 <jm.randrianant65@gmail.com>
2024-01-20 11:12:07 +0800

    refactor

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -3,56 +3,60 @@ const Article = require("../models/article.model");
 const createArticle = async (req, res) => {
   const { title, content } = req.body;
 
-  const article = new Article({
+  const newArticle = new Article({
     title,
     content,
   });
 
-  const newArticle = await article.save();
+  const article = await newArticle.save();
 
-  return res.status(200).json({ success: true, newArticle });
+  return res.status(200).json({ success: true, article });
 };
 
 const deleteArticle = async (req, res) => {
   const { id } = req.params;
 
-  const deletedArticle = await Article.findByIdAndDelete(id);
+  const article = await Article.findByIdAndDelete(id);
 
-  return res.status(200).json({ success: true, deletedArticle });
+  return res.status(200).json({ success: true, article });
 };
 
 const updateArticle = async (req, res) => {
-  const { id } = req.params;
-  const { title, content } = req.body;
+  // try {
+    const { id } = req.params;
+    const { title, content } = req.body;
 
-  const updatedArticle = await Article.findByIdAndUpdate(
-    id,
-    {
-      title,
-      content,
-    },
-    { new: true }
-  );
+    const updatedArticle = await Article.findByIdAndUpdate(
+      id,
+      {
+        title,
+        content,
+      },
+      { new: true }
+    );
 
-  return res.status(200).json({ success: true, article: updatedArticle });
-};
+    return res.status(200).json({ success: true, article: updatedArticle });
+//   } catch (error) {
+//     return res.status(404).json({ success: false, error: error.message });
+  }
+// // };
 
 const getArticle = async (req, res) => {
   try {
     const { id } = req.params;
-    const getArticle = await Article.findById(id);
+    const article = await Article.findById(id);
 
     if (!getArticle) {
       throw new Error("article with the id " + id + " does not exist");
     }
-
-    return res.status(200).json({ success: true, getArticle });
+    return res.status(200).json({ success: true, article });
   } catch (error) {
     return res.status(400).json({ success: false, error: error.message });
   }
 };
 
 const getArticles = async (req, res) => {
+  // throw new Error("Hi from server !!");
   const articles = await Article.find({});
   return res.status(200).json({ success: true, articles });
 };
